43f10ef331f92a7ea0b395b46d84916537446c8c
Sands-45 <sandsqa@hotmail.com>
2021-08-23 19:50:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-m7y1yk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-m7y1yk)
---
+++ script.js
@@ -0,0 +1,7 @@
+let galleryItems = document.querySelectorAll(".gal-img");
+galleryItems.forEach((item)=>{
+item.addEventListener("click",()=>{
+galleryItems.forEach(item => item.classList.remove("active"));
+item.classList.add("active");
+});
+});
